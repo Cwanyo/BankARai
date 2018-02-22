6b66ee3546217d74f118cfffd02b6e62d2f7b605
cwan <manwan_444@hotmail.com>
2018-02-23 00:06:12 +0700

    add view classes

---
+++ app/src/main/java/com/thewiz/bankarai/views/OverlayView.java
@@ -0,0 +1,46 @@
+package com.thewiz.bankarai.views;
+
+/**
+ * Created by C.wan_yo on 23-Feb-18.
+ */
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * A simple View providing a render callback to other classes.
+ */
+public class OverlayView extends View {
+
+    private final List<DrawCallback> callbacks = new LinkedList<DrawCallback>();
+
+    public OverlayView(final Context context, final AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    /**
+     * Interface defining the callback for client classes.
+     */
+    public interface DrawCallback {
+        public void drawCallback(final Canvas canvas);
+    }
+
+    public void addCallback(final DrawCallback callback) {
+        callbacks.add(callback);
+    }
+
+    @SuppressLint("MissingSuperCall")
+    @Override
+    public synchronized void draw(final Canvas canvas) {
+        for (final DrawCallback callback : callbacks) {
+            callback.drawCallback(canvas);
+        }
+    }
+
+}
